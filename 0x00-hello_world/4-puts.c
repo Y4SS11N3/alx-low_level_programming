661f2676ac51e5cc591986f2df4ec361f4472bf4
YASSINE <contact@yassine.fun>
2023-10-05 15:01:25 -0700

    fix

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: This program uses the puts function.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Prints a string
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("Size of a char: %d byte(s)\n", size of(char));
+	printf("Size of an int: %d byte(s)\n", size of(int));
+	printf("Size of a long int: %d byte(s)\n", size of(long int));
+	printf("Size of a long long int: %d byte(s)\n", size of(lon long int));
+	printf("Size of a float: %d byte(s)\n", size of(float));
+	return (0);
+}
