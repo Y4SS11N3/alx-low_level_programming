320363f05a3fcd88b70a0534c96f430f76a5a7b5
YASSINE <contact@yassine.fun>
2023-10-10 12:52:40 -0700

    fix

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Program's Launching pad
+ *
+ * Return: 0 if the program executes successfully, otherwise no
+ */
+int main(void)
+{
+	char TxT[] = "_putchar"; /* text to print */
+	int idx; /* index for iteration */
+
+	for (idx = 0; TxT[idx] != '\0'; idx++)
+	{
+		putchar(TxT[idx]);
+	}
+	putchar('\n');
+	return (0);
+}
+
