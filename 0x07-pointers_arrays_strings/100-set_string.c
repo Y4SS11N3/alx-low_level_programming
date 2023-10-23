705fd702ee2ccc2eac5058048c3a7f3b850e8e31
YASSINE <contact@yassine.fun>
2023-10-23 12:54:48 -0700

    fix

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * set_string - assigns new value to pointer
+ * @s: source pointer
+ * @to: target pointer
+ * Return: void
+ */
+void set_string(char **s, char *to)
+{
+	*s = to; /* assign to to *s */
+}
+
