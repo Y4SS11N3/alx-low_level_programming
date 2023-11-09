5e5bb5466eb12ea9363443829ed742b64709322c
YASSINE <contact@yassine.fun>
2023-11-09 13:49:52 -0800

    fix

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,45 +1,52 @@
 #include "variadic_functions.h"
 
-/**
- * print_all - displays anything.
+/*
+ * print_all - exhibits any type of data.
  * @format: a list of types of arguments passed to the function.
  *
  * Return: nothing.
  */
 void print_all(const char * const format, ...)
 {
+	/* 'valist' is the variable argument list */
 	va_list valist;
-	/* idx: index for iteration */
-	unsigned int idx = 0, j, flag = 0;
-	/* txt: current string */
+	/* 'idx' is the index for iteration, 'jdx' is the secondary index for iteration, 'flag' is a flag variable */
+	unsigned int idx = 0, jdx, flag = 0;
+	/* 'txt' is a pointer to a string */
 	char *txt;
+	/* 't_arg' is a list of argument types */
 	const char t_arg[] = "cifs";
 
 	va_start(valist, format);
 	while (format && format[idx])
 	{
-		j = 0;
-		while (t_arg[j])
+		jdx = 0;
+		while (t_arg[jdx])
 		{
-			if (format[idx] == t_arg[j] && flag)
+			if (format[idx] == t_arg[jdx] && flag)
 			{
 				printf(", ");
 				break;
-			} j++;
+			}
+			jdx++;
 		}
 		switch (format[idx])
 		{
 		case 'c':
-			printf("%c", va_arg(valist, int)), flag = 1;
+			printf("%c", va_arg(valist, int));
+			flag = 1;
 			break;
 		case 'i':
-			printf("%d", va_arg(valist, int)), flag = 1;
+			printf("%d", va_arg(valist, int));
+			flag = 1;
 			break;
 		case 'f':
-			printf("%f", va_arg(valist, double)), flag = 1;
+			printf("%f", va_arg(valist, double));
+			flag = 1;
 			break;
 		case 's':
-			txt = va_arg(valist, char *), flag = 1;
+			txt = va_arg(valist, char *);
+			flag = 1;
 			if (!txt)
 			{
 				printf("(nil)");
@@ -47,7 +54,9 @@ void print_all(const char * const format, ...)
 			}
 			printf("%s", txt);
 			break;
-		} idx++;
+		}
+		idx++;
 	}
-	printf("\n"), va_end(valist);
+	printf("\n");
+	va_end(valist);
 }
