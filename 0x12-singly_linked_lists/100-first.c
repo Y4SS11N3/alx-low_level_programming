4860b636a6d803587f2798c35763b2030736f3b9
YASSINE <contact@yassine.fun>
2023-11-16 13:41:56 -0800

    Task 5 & Task 6

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+/**
+ * callfirst - executes automatically before the main function
+ * This function is designed to print a predetermined message
+ * before the program's main logic begins.
+ */
+void callfirst(void)
+{
+	printf("You're beat! and yet, you must allow,\n"
+		"I bore my house upon my back!\n"
+		);
+}
