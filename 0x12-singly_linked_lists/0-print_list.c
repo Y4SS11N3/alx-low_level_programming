81825e0147f879386156774620555996f0f7c84e
YASSINE <contact@yassine.fun>
2023-11-16 13:28:06 -0800

    Task 0 - Print list elements

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,27 +7,27 @@
  */
 size_t print_list(const list_t *h)
 {
-    size_t ctr = 0; /* Node counter */
+	size_t ctr = 0; /* Node counter */
 
-    if (h == NULL)
-        return (0);
-    while (h->next)
-    {
-        if (!h->str)
-            printf("[0] (nil)\n");
-        else
-            printf("[%u] %s\n", h->len, h->str);
-        h = h->next;
-        ctr++;
-    }
-    /* Print the last node if it exists */
-    if (h)
-    {
-        if (!h->str)
-            printf("[0] (nil)\n");
-        else
-            printf("[%u] %s\n", h->len, h->str);
-        ctr++;
-    }
-    return (ctr);
+	if (h == NULL)
+		return (0);
+	while (h->next)
+	{
+		if (!h->str)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", h->len, h->str);
+		h = h->next;
+		ctr++;
+	}
+	/* Print the last node if it exists */
+	if (h)
+	{
+		if (!h->str)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", h->len, h->str);
+		ctr++;
+	}
+	return (ctr);
 }
